56e9af2839ba0da35bc8b0f4aab91779a26d1d75
Maxim Elushev <maxim.elushev@yandex.ru>
2023-01-28 23:52:55 +0300

    01.py

---
+++ Algorithms/01.py
@@ -0,0 +1,17 @@
+def binary_search(arr, x):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+arr = list(map(int, input().split(',')))
+x = int(input())
+print(binary_search(arr, x))
